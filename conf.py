1017fcd053f61ff0a7b4607b6c376bba0d36a3a3
cyjseagull <1547158861@qq.com>
2018-08-02 10:32:09 +0800

    try to fix

--- conf.py
+++ conf.py
@@ -306,12 +306,12 @@ texinfo_documents = [
 #]
 
 #github_doc_root = 'https://github.com/rtfd/recommonmark/tree/master/doc/'
-def setup(app):
+#def setup(app):
 #    app.add_config_value('recommonmark_config', {
 #            'url_resolver': lambda url: github_doc_root + url,
 #            'auto_toc_tree_section': 'Contents',
 #            }, True)
-    app.add_transform(AutoStructify)
+#    app.add_transform(AutoStructify)
 
 # The suffix of source filenames.
 source_suffix = ['.rst', '.md']
