337e6c419ef4f6fef7b4f2bb656fd60b7f71542d
cyjseagull <1547158861@qq.com>
2018-09-17 20:09:26 +0800

    modify

--- conf.py
+++ conf.py
@@ -324,7 +324,6 @@ def setup(app):
         'auto_toc_tree_section': 'Contents',
         'enable_eval_rst': True,
         'enable_auto_doc_ref': True,
-        'enable_table_extension': True,
 	}, True)
 
     app.add_transform(AutoStructify)
