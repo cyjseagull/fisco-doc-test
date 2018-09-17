30102aa4fd7186e96efaea81ab3161e039a689a6
cyjseagull <1547158861@qq.com>
2018-09-17 17:43:14 +0800

    modify CommonMark to commonmark

--- conf.py
+++ conf.py
@@ -19,9 +19,11 @@ import shlex
 
 #sys.path.insert(0, os.path.abspath('..'))
 import sphinx_rtd_theme
+from commonmark import Parser
 from recommonmark.parser import CommonMarkParser
 from recommonmark.transform import AutoStructify
-
+from commonmark import DocParser
+from docutils import nodes
 
 # The suffix of source filenames.
 source_parsers = {
